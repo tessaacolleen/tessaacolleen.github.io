8e86629cbd6b6669e75ee09a287fca1f3c347ea5
tessaacolleen <tessawaldhoff@gmail.com>
2021-11-13 15:57:25 -0600

    data circles

--- firstViz.js
+++ firstViz.js
@@ -1,5 +1,7 @@
 console.log("Hello")
 
+data = [30, 70]
+
 let width = 970
 let height = 700
 var svg = d3.select("#viz")
@@ -22,4 +24,18 @@ svg.append("rect")
 .attr("height", 80)
 .attr("fill", "lightblue")
 
+svg.append("rect")
+.attr("cx", 400)
+.attr("cy", 200)
+.attr("r", 50)
+.attr("fill", "green")
 
+all = svg.selectAll()
+.data(data)
+.enter()
+
+all.append("circle")
+.attr("cx", (d, i) => (i+1) * 100)
+.attr("cy", 200)
+.attr("r", (d, i) => d)
+.attr("fill", "lavender")
