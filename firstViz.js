69a2cf8811b4cd2a3cab801f7fa66407f20aaa54
tessaacolleen <tessawaldhoff@gmail.com>
2021-11-13 15:29:00 -0600

    js code

--- firstViz.js
+++ firstViz.js
@@ -1,1 +1,10 @@
 console.log("Hello!")
+
+let width = 970
+let height = 700
+var svg = d3.select("#viz")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .style("width", width)
+            .style("height", height)
